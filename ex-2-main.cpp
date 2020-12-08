ab02569d5a994ea4de0c3692df285c760b54920a
ShokutoFukada <ht18a085@oecu.jp>
2020-12-08 13:00:55 +0900

    ex-2-main.cppのスケルトンプログラムの作成

---
+++ ex-2-main.cpp
@@ -0,0 +1,71 @@
+// ex_2_main.cpp
+#include <iostream>
+using namespace std;
+
+namespace cpp2 {
+	/* --------------------------------------------------------------------- */
+	/*
+	mcxi
+	mcxi 記法を解析するクラスです。
+	*/
+	/* --------------------------------------------------------------------- */
+	class mcxi {
+
+	};
+
+}
+
+int main()
+{
+	cpp2::mcxi a0("xi");
+	cpp2::mcxi b0("x9i");
+	auto result0 = a0 + b0;
+	std::cout << "3x" << " " << result0.to_string() << std::endl;
+
+	cpp2::mcxi a1("i");
+	cpp2::mcxi b1("9i");
+	auto result1 = a1 + b1;
+	std::cout << "x" << " " << result1.to_string() << std::endl;
+
+	cpp2::mcxi a2("c2x2i");
+	cpp2::mcxi b2("4c8x8i");
+	auto result2 = a2 + b2;
+	std::cout << "6cx" << " " << result2.to_string() << std::endl;
+
+	cpp2::mcxi a3("m2ci");
+	cpp2::mcxi b3("4m7c9x8i");
+	auto result3 = a3 + b3;
+	std::cout << "5m9c9x9i" << " " << result3.to_string() << std::endl;
+
+	cpp2::mcxi a4("9c9x9i");
+	cpp2::mcxi b4("i");
+	auto result4 = a4 + b4;
+	std::cout << "m" << " " << result4.to_string() << std::endl;
+
+	cpp2::mcxi a5("i");
+	cpp2::mcxi b5("9m9c9x8i");
+	auto result5 = a5 + b5;
+	std::cout << "9m9c9x9i" << " " << result5.to_string() << std::endl;
+
+	cpp2::mcxi a6("m");
+	cpp2::mcxi b6("i");
+	auto result6 = a6 + b6;
+	std::cout << "mi" << " " << result6.to_string() << std::endl;
+
+	cpp2::mcxi a7("i");
+	cpp2::mcxi b7("m");
+	auto result7 = a7 + b7;
+	std::cout << "mi" << " " << result7.to_string() << std::endl;
+
+	cpp2::mcxi a8("m9i");
+	cpp2::mcxi b8("i");
+	auto result8 = a8 + b8;
+	std::cout << "mx" << " " << result8.to_string() << std::endl;
+
+	cpp2::mcxi a9("9m8c7xi");
+	cpp2::mcxi b9("c2x8i");
+	cpp2::mcxi result9 = a9 + b9;
+	std::cout << "9m9c9x9i" << " " << result9.to_string() << std::endl;
+
+    return 0;
+}
